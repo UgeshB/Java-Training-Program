f63b40851d5f2b6e0a94d3ef2be60dde307b0623
UgeshB <49775802+UgeshB@users.noreply.github.com>
2019-05-07 18:35:50 +0530

    Add files via upload

--- PostJavaTraining/src/sql/result/Main.java
+++ PostJavaTraining/src/sql/result/Main.java
@@ -19,15 +19,22 @@ public class Main {
 			Class.forName("com.mysql.jdbc.Driver");
 			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb",USER,PASS); 
 			stmt = con.createStatement(); 
-			String sql= "SELECT CONCAT(firstName,lastName)as Name,p.PaymentMode as PaymentMode ,p.amount as Amount ,p.Month AS Month from mydb.user as u  join mydb.payment as p where p.user_id = u.id"; 
+			String sql="SELECT CONCAT(firstName,' ',lastName)as 'User Full Name',p.Month AS Month,p.PaymentMode as 'Payment Mode' ,sum(p.amount) as 'Total Amount' "
+					+ "from mydb.user as u  join mydb.payment as p "
+					+ "where p.user_id = u.id "
+					+ "group by p.PaymentMode,p.Month";
 			ResultSet rs= stmt.executeQuery(sql);
 			System.out.println(rs);
-		
+			System.out.print("User Full Name");
+			System.out.print("\tMonth");
+			System.out.print("\tPayment Mode");
+			System.out.print("\tTotal amount");
+			System.out.println();
 			while(rs.next()) {
-				System.out.print(" Name :  "+rs.getString("Name"));
-				System.out.print(" PaymentMode :  "+rs.getString("PaymentMode"));
-				System.out.print(" Aamount :  "+rs.getString("Amount"));
-				System.out.print(" Month :  "+rs.getString("Month"));
+				System.out.print(rs.getString("User Full Name"));
+				System.out.print("\t"+rs.getString("Month"));
+				System.out.print("\t"+rs.getString("Payment Mode"));
+				System.out.print("\t\t"+rs.getString("Total Amount"));
 				System.out.println();
 			}
 			}
--- PostJavaTraining/src/vocher/card/validation/Main.java
+++ PostJavaTraining/src/vocher/card/validation/Main.java
@@ -9,31 +9,44 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		ArrayList<Integer> al =new ArrayList<Integer>();
-		ArrayList<Integer> al1 =new ArrayList<Integer>();
+		ArrayList<Integer> digits =new ArrayList<Integer>();
+		ArrayList<Integer> doubleDigits =new ArrayList<Integer>();
 		System.out.println("Enter digis");
-		int dig=Integer.parseInt(sc.nextLine());
+		String dig=sc.nextLine();
 		int j=1,sum=0;
-		while(dig>0) {
-			int digit = dig%10;
-			al.add(digit);
-			dig=dig/10;
+		int length=dig.length();
+		for(int i=length-1;i>=0;i--) {
+			int digit=Integer.parseInt(dig.charAt(i)+"");
+			digits.add(digit);
 			if(j%2==0) {
 				digit=digit*2;
+				if(digit>9) {
+					int one =digit%10;
+					int second=digit/10;
+					digit=one+second;
+				}	
 			}
-			 al1.add(digit);
+			doubleDigits.add(digit);
 			sum=sum+digit;
 			j++;
 		}
-		System.out.println("digits:"+al);
-		System.out.println("After double"+al1);
-		System.out.println("sum:"+sum);
-
-
+		System.out.print("Digits \t:");
+		for(int i=length-1;i>=0;i--) {
+		System.out.print(" "+digits.get(i));
+		}
+		System.out.println();
+		System.out.print("After Doubling: ");
+		for(int i=length-1;i>=0;i--) {
+			System.out.print(" "+doubleDigits.get(i));
+			}
+		System.out.println();
+		System.out.println("sum of digits:"+sum);
+		if(sum%10==0) {
+			System.out.println("Valied Vocher Card");
+		}
+		else {
+			System.out.println("Invaid Vocher card");
+		}
 	}
-
-
-
 }
